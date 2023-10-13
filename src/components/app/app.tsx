df741328613f4dbde94e10eb6aab8a20b8443416
Mardari Ilie <soaccentru@gmail.com>
2023-10-13 23:27:54 +0200

    Add main screen

---
+++ src/components/app/app.tsx
@@ -0,0 +1,13 @@
+import Main from '../../pages/main/main';
+
+ type AppProps = {
+   offers: number;
+ }
+
+function App({ offers }: AppProps): JSX.Element {
+  return (
+    <Main offers={ offers } />
+  );
+}
+
+export default App;
---
+++ src/const.ts
@@ -0,0 +1,3 @@
+export const enum Settings {
+  offers = 300
+}
