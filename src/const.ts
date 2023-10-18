9d9e8b3cb9cf99aa9a2bf5e58098cd6088915dd9
Mardari Ilie <soaccentru@gmail.com>
2023-10-19 00:14:48 +0200

    Add routes to the application

--- src/const.ts
+++ src/const.ts
@@ -1,3 +1,15 @@
 export const Settings = {
   offers: 300,
 } as const;
+
+export const enum AppRoute {
+  Login = '/login',
+  Favorites = '/favorites',
+  Offer = '/offer/'
+}
+
+export const enum AuthorizationStatus {
+  Auth = 'AUTH',
+  NoAuth = 'NO_AUTH',
+  Unknown = 'UNKNOWN',
+}
